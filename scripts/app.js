a5caeeda7e6a7b064179f30524ed32e63718a5ec
Kasonde Besa <61287639+KayBesa@users.noreply.github.com>
2023-10-24 17:45:15 -0400

    Create app.js

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+/**
+    Name: Kasonde Besa
+    Script: app.js
+    Description: Controls functionaly of simple JavaScript webApp of rock-paper-scissors that
+                 a player can play against AI.
+    Date: October 23rd, 2023
+*/
